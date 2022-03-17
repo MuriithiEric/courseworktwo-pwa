b866dd35e5810be2ed3f507add92d4e0aa1c199e
MuriithiEric <56841703+MuriithiEric@users.noreply.github.com>
2022-03-18 03:32:24 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const http = require("http");
+const app = express();
+const port = process.env.PORT || 3000;
