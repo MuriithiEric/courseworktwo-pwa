3b7e817b7fedecc4e3e7a3fbdffdeeda56fccf03
Eric Muriithi <emuriithi42@gmail.com>
2022-03-18 03:58:52 +0400

    Third Commit, params middleware

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const http = require("http");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 // MIDDLEWARES
 // use Parser Middleware
-app.use(express.json())
+app.use(express.json());
+
+// use Logger middleware
+app.use(function (req, res, next) {
+  console.log("Request IP: " + req.url);
+  console.log("Request date: " + new Date());
+  return next();
+});
+
+// use param middleware
+app.param("collectionName", function (req, res, next, collectionName) {
+  req.collection = db.collection(collectionName);
+  return next();
+});
